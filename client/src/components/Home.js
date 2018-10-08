51f19f70d0341241339d0fbb11b7766c660d59d3
Nate <nathanestes@gmail.com>
2018-10-08 10:23:53 -0600

    coin crd actions

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
+import axios from 'axios'
+
 
 class Home extends Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <>
+      </>
     );
   }
 }
 
 export default Home;
+
+
+
+state = { coin: {} 
+}
+
+clicky = () => {
+  axios.post('/api/coins?coin=btc')
+    .then( res => { debugger } )
+}
+
+clicky2 = () => {
+  axios.get('/api/coins')
+    .then( res => { debugger } ) 
+}
+
+<Button onClick={this.clicky}>
+        Click Me!
+      </Button>
+      <Button onClick={this.clicky2}>
+        Click Me!
+      </Button>
